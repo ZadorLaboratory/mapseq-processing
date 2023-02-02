991d2d439531d7be115618e832ff36addfff8e77
jhover <jhover@transit.cshl.edu>
2023-02-02 10:29:02 -0500

    Update.

--- test/m205_pp8nt1.sh
+++ test/m205_pp8nt1.sh
@@ -15,21 +15,21 @@ BCFILE="barcode_v2.txt"
   
 #strip fastq files and clip sequences
 
-awk "NR%4==2" ${FQ1}.fastq | cut -b 1-32 > ${FQ1}_stripped.txt #barcode+YY
+awk "NR%4==2" ${FQ1}.fastq | cut -b 1-32 > ${FQ1}.stripped.txt # barcode + YY
 rm ${FQ1}.fastq
-awk "NR%4==2" ${FQ2}.fastq | cut -b 1-20 > ${FQ2}_stripped.txt #12nt tag+ 8nt index
+awk "NR%4==2" ${FQ2}.fastq | cut -b 1-20 > ${FQ2}.stripped.txt #12nt tag + 8nt index
 rm ${FQ2}.fastq
 
 
 #make a new file that contains only one sequence per sequenced cluster
-paste -d '\0' ${FQ1}_stripped.txt ${FQ2}_stripped.txt > ${SAMPLE}_PE.txt
+paste -d '\0' ${FQ1}.stripped.txt ${FQ2}.stripped.txt > ${SAMPLE}.paired.txt
 
 
 #split dataset according to inline indexes using fastx toolkit; this by default allows up to 1 missmatch. we could go higher if we want, though maybe not neccessary
 mkdir barcodesplitter
 cd barcodesplitter
 
-nl ../${SAMPLE}_PE.txt |awk '{print ">" $1 "\n" $2}'| fastx_barcode_splitter.pl --bcfile ../${BCFILE} --prefix ../barcodesplitter/ --eol --exact
+nl ../${SAMPLE}.paired.txt |awk '{print ">" $1 "\n" $2}'| fastx_barcode_splitter.pl --bcfile ../${BCFILE} --prefix ../barcodesplitter/ --eol --exact
 
 #from here on, do everything for every sample individually
 
@@ -37,15 +37,12 @@ nl ../${SAMPLE}_PE.txt |awk '{print ">" $1 "\n" $2}'| fastx_barcode_splitter.pl 
 BCidx=($(seq 0 1 28)) 
 for i in {1..28}; do #this number should be exactly the same as the total RT primer number used in BCidx
 	#filter out reads with Ns, cut off indexes and unique datafiles
-	awk "NR%2==0" BC${BCidx[$i]} | grep -v N | cut -b 1-44 | sort | uniq -c | sort -nr > ${SAMPLE}processedBC${BCidx[$i]}.txt
-	#split output files into two files per index, one that is containing the read counts of each unique sequnce, the other the unique sequences themselves.
-	awk '{print $1}' ${SAMPLE}processedBC${BCidx[$i]}.txt > ${SAMPLE}BC${BCidx[$i]}_counts.txt
-	awk '{print $2}' ${SAMPLE}processedBC${BCidx[$i]}.txt > ${SAMPLE}_BC${BCidx[$i]}seq.txt
+	awk "NR%2==0" BC${BCidx[$i]} | grep -v N | cut -b 1-44 | sort | uniq -c | sort -nr > ${SAMPLE}.processed.BC${BCidx[$i]}.txt
+	#split output files into two files per index, one that is containing the read counts of each unique sequence, the other the unique sequences themselves.
+	awk '{print $1}' ${SAMPLE}.processed.BC${BCidx[$i]}.txt > ${SAMPLE}.BC${BCidx[$i]}.counts.txt
+	awk '{print $2}' ${SAMPLE}.processed.BC${BCidx[$i]}.txt > ${SAMPLE}.BC${BCidx[$i]}.seq.txt
 done
 
 
 mkdir thresholds
 cd thresholds
-
-
-
--- test/m205_pp8nt2.sh
+++ test/m205_pp8nt2.sh
@@ -1,10 +1,5 @@
-#!/bin/bash 
-# run job in the current working directory where qsub is executed from
-#$ -cwd
-# specify that the job requires 4GB of memory
-#$ -l m_mem_free=4G
-#example preprocessing of seqeuncing libary ZL145
-
+#!/bin/bash -x
+#example preprocessing of sequencing libary M205
 
 FQ1="M205_HZ_S1_R1_001"
 FQ2="M205_HZ_S1_R2_001"
@@ -12,35 +7,35 @@ SAMPLE="M205_HZ_S1"
 
 cd barcodesplitter/thresholds
  
-#pick thresholds from matlab plots, based on a steep drop of the 32+12 read counts. 
-# avoids too many PCR and sequnecning errors in the data themselves. note, bash seems 
+# pick thresholds from matlab plots, based on a steep drop of the 32+12 read counts. 
+# avoids too many PCR and sequencing errors in the data themselves. Note, bash seems 
 # to start counting at 0, so put a random number at the first position of this array, 
 # to get the order right.
 
 BCidx=($(seq 0 1 28))
-threshold=(2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 ) #you can put more than the actual RT number
+threshold=(2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 ) #you can put more than the actual RT number
 
-#just do a quick and dirty collpase around the UMI tags
+#just do a quick and dirty collapse around the UMI tags
 for i in {1..28}; do
     j=${threshold[$i]} 
-    echo $j
-    if [ "$j" != "1" ];then
-	awk '$1< '$j' {print NR}' ../${SAMPLE}BC${BCidx[$i]}_counts.txt | head -n 1 >t
-	thresh=$(cat t)
-	echo $thresh
-	head ../ZL231_Mseq125_VM_BC${BCidx[$i]}seq.txt -n $thresh | cut -b 1-32 | sort | uniq -c | sort -nr > ${SAMPLE}${i}quickout.txt
-
-   else
-	grep -nr ^${threshold[$i]}$  ../${SAMPLE}BC${BCidx[$i]}_counts.txt -m 1 | cut -f1 -d":" > t
-	thresh=$(cat t)
-	echo $thresh
-
-	head ../${SAMPLE}_BC${BCidx[$i]}seq.txt -n $thresh | cut -b 1-32 | sort | uniq -c | sort -nr > ${SAMPLE}${i}quickout.txt
-   fi
+    #echo " threshval = $j"
+    if [ "$j" != "1" ]; then
+    	echo "awk -v j=$j '$1<j {print NR}' ../${SAMPLE}.BC${BCidx[$i]}.counts.txt | head -n 1 > t$i"
+		awk -v j=$j '$1<j {print NR}' ../${SAMPLE}.BC${BCidx[$i]}.counts.txt | head -n 1 > t$i
+		thresh=$(cat t$i)
+		echo "thresh = ${thresh}"
+		echo "head  -n $thresh ../${SAMPLE}.BC${BCidx[$i]}.seq.txt | cut -b 1-32 | sort | uniq -c | sort -nr > ${SAMPLE}.${i}.quickout.txt"
+		head  -n $thresh ../${SAMPLE}.BC${BCidx[$i]}.seq.txt | cut -b 1-32 | sort | uniq -c | sort -nr > ${SAMPLE}.${i}.quickout.txt
+    else
+		grep -nr ^${threshold[$i]}$  ../${SAMPLE}.BC${BCidx[$i]}.counts.txt -m 1 | cut -f1 -d":" > t$i
+		thresh=$(cat t$i)
+		echo "thresh = ${thresh}"
+		head ../${SAMPLE}.BC${BCidx[$i]}.seq.txt -n $thresh | cut -b 1-32 | sort | uniq -c | sort -nr > ${SAMPLE}.${i}.quickout.txt
+    fi
 done
 
 #ok, thats a lot of preprocessing done. Now we have to error correct these sequences. 
-# The all against all mapping of barcodes in using bowtie is done in the command line. 
+# The all against all mapping of barcodes using bowtie is done on the command line. 
 # after this we move into MATLAB.
 
 mkdir indexes
@@ -48,50 +43,53 @@ mkdir indexes
 for i in {1..28}
 do
 	echo $i
-	in=ZL231_Mseq125_VM${i}quickout.txt
+	in=${SAMPLE}.${i}.quickout.txt
+	echo "handling real ${in} ...}"
 	#split off real barcodes from spike-ins
 
-	grep -v 'CGTCAGTC$' $in | grep '[TC][TC]$' > ZL231_Mseq125_VMBC${i}_quickprocessed.txt
-	awk '{print $1}' ZL231_Mseq125_VMBC${i}_quickprocessed.txt > ZL231_Mseq125_VM${i}_counts.txt
-	awk '{print $2}' ZL231_Mseq125_VMBC${i}_quickprocessed.txt > ZL231_Mseq125_VM${i}_seq.txt
+	grep -v 'CGTCAGTC$' $in | grep '[TC][TC]$' > ${SAMPLE}.BC${i}.quickprocessed.txt
+	awk '{print $1}' ${SAMPLE}.BC${i}.quickprocessed.txt > ${SAMPLE}.${i}.counts.txt
+	awk '{print $2}' ${SAMPLE}.BC${i}.quickprocessed.txt > ${SAMPLE}.${i}.seq.txt
 
-
-	nl ZL231_Mseq125_VM${i}_seq.txt | awk '{print ">" $1 "\n" $2}' > ZL231_Mseq125_VM_BC${i}fasta2u.txt; 
-	bowtie-build -q ZL231_Mseq125_VM_BC${i}fasta2u.txt indexes/BC${i}fasta2u; 
-	bowtie -v 3 -p 10 -f --best -a indexes/BC${i}fasta2u ZL231_Mseq125_VM_BC${i}fasta2u.txt bowtiealignment${i}_2u.txt
-	awk '{print $1}' bowtiealignment${i}_2u.txt > bowtie${i}_2u_1.txt;
-	awk '{print $3}' bowtiealignment${i}_2u.txt > bowtie${i}_2u_3.txt
+	nl ${SAMPLE}.${i}.seq.txt | awk '{print ">" $1 "\n" $2}' > ${SAMPLE}.BC${i}.fasta2u.txt 
+	bowtie-build -q ${SAMPLE}.BC${i}.fasta2u.txt indexes/BC${i}fasta2u 
+	bowtie -v 3 -p 10 -f --best -a indexes/BC${i}fasta2u ${SAMPLE}.BC${i}.fasta2u.txt bowtiealign${i}.2u.txt
+	awk '{print $1}' bowtiealign${i}.2u.txt > bowtie${i}.2u.1.txt
+	awk '{print $3}' bowtiealign${i}.2u.txt > bowtie${i}.2u.3.txt
 done
 
 
 # now deal with spike ins
-for i in {1..26}; do 
-	echo $i; in=ZL231_Mseq125_VM${i}quickout.txt 
-	grep 'CGTCAGTC$' $in > ZL231_Mseq125_VMspikes${i}_quickprocessed.txt; 
-	awk '{print $1}' ZL231_Mseq125_VMspikes${i}_quickprocessed.txt > ZL231_Mseq125_VMspikes${i}_counts.txt; 
-	awk '{print $2}' ZL231_Mseq125_VMspikes${i}_quickprocessed.txt > ZL231_Mseq125_VMspikes${i}_seq.txt;  
-	nl ZL231_Mseq125_VMspikes${i}_seq.txt | awk '{print ">" $1 "\n" $2}' > ZL231_Mseq125_VM_spikes${i}fasta2u.txt; 
-	bowtie-build -q ZL231_Mseq125_VM_spikes${i}fasta2u.txt indexes/spikes${i}fasta2u 
-	bowtie -v 3 -p 10 -f --best -a indexes/spikes${i}fasta2u ZL231_Mseq125_VM_spikes${i}fasta2u.txt bowtiealignmentspikes${i}_2u.txt; 
-	awk '{print $1}' bowtiealignmentspikes${i}_2u.txt > bowtiespikes${i}_2u_1.txt;
-	awk '{print $3}' bowtiealignmentspikes${i}_2u.txt > bowtiespikes${i}_2u_3.txt; 
+for i in {1..28}; do 
+	echo $i
+	in=${SAMPLE}.${i}.quickout.txt 
+	echo "handling spikeins ${in} ...}"	
+	
+	grep 'CGTCAGTC$' $in > ${SAMPLE}.spikes.${i}.quickprocessed.txt 
+	awk '{print $1}' ${SAMPLE}.spikes.${i}.quickprocessed.txt > ${SAMPLE}.spikes.${i}.counts.txt 
+	awk '{print $2}' ${SAMPLE}.spikes.${i}.quickprocessed.txt > ${SAMPLE}.spikes.${i}.seq.txt  
+	nl ${SAMPLE}.spikes.${i}.seq.txt | awk '{print ">" $1 "\n" $2}' > ${SAMPLE}.spikes.${i}.fasta2u.txt 
+	bowtie-build -q ${SAMPLE}.spikes.${i}.fasta2u.txt indexes/spikes${i}fasta2u 
+	bowtie -v 3 -p 10 -f --best -a indexes/spikes${i}fasta2u ${SAMPLE}.spikes.${i}.fasta2u.txt bowtiealignspikes.${i}.2u.txt 
+	awk '{print $1}' bowtiealign.spikes.${i}.2u.txt > bowtiespikes.${i}.2u.1.txt
+	awk '{print $3}' bowtiealign.spikes.${i}.2u.txt > bowtiespikes.${i}.2u.3.txt 
 done
 
 # L1 barcodes
-for i in {1..26}
-do
+for i in {1..28}; do
 	echo $i
-	in=ZL231_Mseq125_VM${i}quickout.txt
-
-	#split off real barcodes from spike-ins
-	grep -v 'CGTCAGTC$' $in | grep '[AG][AG]$' > ZL231_Mseq125_VMBC${i}_quickprocessedL1.txt
-	awk '{print $1}' ZL231_Mseq125_VMBC${i}_quickprocessedL1.txt > ZL231_Mseq125_VM${i}_countsL1.txt
-	awk '{print $2}' ZL231_Mseq125_VMBC${i}_quickprocessedL1.txt > ZL231_Mseq125_VM${i}_seqL1.txt
+	in=${SAMPLE}.${i}.quickout.txt
+	echo "handling L1 ${in} ...}"	
 	
-	nl ZL231_Mseq125_VM${i}_seqL1.txt | awk '{print ">" $1 "\n" $2}' > ZL231_Mseq125_VM_BC${i}fasta2uL1.txt;
-	bowtie-build -q ZL231_Mseq125_VM_BC${i}fasta2uL1.txt indexes/BC${i}fasta2uL1;
-	bowtie -v 3 -p 10 -f --best -a indexes/BC${i}fasta2uL1 ZL231_Mseq125_VM_BC${i}fasta2uL1.txt bowtiealignment${i}_2uL1.txt
-	awk '{print $1}' bowtiealignment${i}_2uL1.txt > bowtie${i}_2u_1L1.txt;awk '{print $3}' bowtiealignment${i}_2uL1.txt > bowtie${i}_2u_3L1.txt
+	#split off real barcodes from spike-ins
+	grep -v 'CGTCAGTC$' $in | grep '[AG][AG]$' > ${SAMPLE}.BC${i}.quickprocessed.L1.txt
+	awk '{print $1}' ${SAMPLE}.BC${i}.quickprocessed.L1.txt > ${SAMPLE}.${i}.counts.L1.txt
+	awk '{print $2}' ${SAMPLE}.BC${i}.quickprocessed.L1.txt > ${SAMPLE}.${i}.seq.L1.txt
+	
+	nl ${SAMPLE}.${i}.seq.L1.txt | awk '{print ">" $1 "\n" $2}' > ${SAMPLE}.BC${i}.fasta2u.L1.txt
+	bowtie-build -q ${SAMPLE}.BC${i}.fasta2u.L1.txt indexes/BC${i}fasta2uL1
+	bowtie -v 3 -p 10 -f --best -a indexes/BC${i}fasta2uL1 ${SAMPLE}.BC${i}.fasta2u.L1.txt bowtiealign.${i}.2u.L1.txt
+	awk '{print $1}' bowtiealign.${i}.2u.L1.txt > bowtie${i}_2u_1L1.txt;awk '{print $3}' bowtiealign.${i}.2u.L1.txt > bowtie.${i}.2u.3.L1.txt
 done
 
 
