b1c870ea7bdd5f421aa06de964faebd48b7ecb50
jhover <jhover@10.183.1.243>
2023-10-23 18:18:00 -0400

    Update.

--- mapseq/utils.py
+++ mapseq/utils.py
@@ -376,7 +376,11 @@ def load_df(filepath):
     df = pd.read_csv(filepath, sep='\t', index_col=0, keep_default_na=False, dtype =str, comment="#")
     df.fillna(value='', inplace=True)
     df = df.astype('str', copy=False)
-    df = df.astype(int, copy=False)
+    for col in df.columns:
+        try:
+            df[col] = df[col].astype(int, copy=False)
+        except:
+            pass 
     return df
 
 def merge_dfs( dflist):
