e9605607e6136c30e3a80df45d94ce3c7f804bf4
hover <hover@localhost>
2024-10-17 13:53:38 -0400

    Update.

--- misc/htna2024/setup1.sh
+++ misc/htna2024/setup1.sh
@@ -5,7 +5,8 @@ cd ; mkdir mapseq ; cd mapseq
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash ./Miniconda3-latest-Linux-x86_64.sh -b -u 
 ~/miniconda/bin/conda init bash
-wget 
+wget https://raw.githubusercontent.com/ZadorLaboratory/mapseq-processing/refs/heads/main/misc/htna2024/setup2.sh
+chmod +x setup2.sh
 exit
 
 
