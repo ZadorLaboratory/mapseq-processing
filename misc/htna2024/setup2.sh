88a00ecb485f4f887e62eb024e9611f90c2b0109
hover <hover@localhost>
2024-10-17 14:04:10 -0400

    Update

--- misc/htna2024/setup2.sh
+++ misc/htna2024/setup2.sh
@@ -4,3 +4,13 @@ wget https://labshare.cshl.edu/shares/mbseq/mapseq/htna2024/M205_sampleinfo.xlsx
 mkdir fastq ; cd fastq
 wget https://labshare.cshl.edu/shares/mbseq/mapseq/htna2024/fastq/M205_HZ_S1_R1_001.fastq.gz
 wget https://labshare.cshl.edu/shares/mbseq/mapseq/htna2024/fastq/M205_HZ_S1_R2_001.fastq.gz
+conda update -y -n base -c defaults conda
+conda config --add channels conda-forge
+conda config --add channels bioconda
+conda create -y -n mapseq python==3.12
+conda activate mapseq
+conda install -y pandas pyarrow ipython jupyter numpy scikit-learn matplotlib seaborn biopython scipy fastcluster openpyxl bowtie2 natsort git paramiko
+mkdir -p ~/git ; cd ~/git
+git clone https://github.com/ZadorLaboratory/mapseq-processing.git
+git clone https://github.com/ZadorLaboratory/mapseq-analysis.git
+cd ~/mapseq
