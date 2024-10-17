fab7d684581907e2f2271da59d8d3c38d21e6abc
hover <hover@localhost>
2024-10-17 15:18:31 -0400

    Update.

--- misc/htna2024/setup3.sh
+++ misc/htna2024/setup3.sh
@@ -3,5 +3,5 @@ conda install -y pandas pyarrow ipython jupyter numpy scikit-learn matplotlib se
 mkdir -p ~/git ; cd ~/git
 git clone https://github.com/ZadorLaboratory/mapseq-processing.git
 git clone https://github.com/ZadorLaboratory/mapseq-analysis.git
-cd ~/mapseq
-wget 
+cd 
+echo "run jupyter notebook, copy URL into browser, navigate to git/mapseq-processing/notebooks"
