72a5324d3947e24cfad74c32663d525153d7e3c7
hover <hover@localhost>
2024-10-17 14:36:48 -0400

    Update.

--- misc/htna2024/setup1.sh
+++ misc/htna2024/setup1.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 #
 # Set up conda
-cd ; mkdir mapseq ; cd mapseq
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O Miniconda3-latest-Linux-x86_64.sh
-bash ./Miniconda3-latest-Linux-x86_64.sh -b -u 
-wget https://raw.githubusercontent.com/ZadorLaboratory/mapseq-processing/refs/heads/main/misc/htna2024/setup2.sh -O setup2.sh
-chmod +x setup2.sh
-wget https://raw.githubusercontent.com/ZadorLaboratory/mapseq-processing/refs/heads/main/misc/htna2024/setup3.sh -O setup3.sh
-chmod +x setup3.sh
+cd ; mkdir mapseq ; cd mapseq ; wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O Miniconda3-latest-Linux-x86_64.sh
+cd mapseq ; bash ./Miniconda3-latest-Linux-x86_64.sh -b -u 
+wget https://raw.githubusercontent.com/ZadorLaboratory/mapseq-processing/refs/heads/main/misc/htna2024/setup2.sh -O setup2.sh ; chmod +x setup2.sh
+wget https://raw.githubusercontent.com/ZadorLaboratory/mapseq-processing/refs/heads/main/misc/htna2024/setup3.sh -O setup3.sh ; chmod +x setup3.sh
 ~/miniconda/bin/conda init bash
 echo "restart shell and run setup2.sh"
 
