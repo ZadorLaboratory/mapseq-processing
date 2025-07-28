483187bb48a619b99392f72b2e268052530d430a
hover <hover@localhost>
2025-07-28 16:22:43 -0400

    Update.

---
+++ attic/hyopil/Bash processing.sh
@@ -0,0 +1,35 @@
+#!/bin/bash 
+#raw files are already demultiplexed by the core according to index i5 i7 pairs
+#Give name for your experiment
+Prefix='dat8'
+#prepare CSI txt file
+#Check whether the fastq.gz file name fits the below script and check CSI (or SSI)for i in {1..96};do    zcat raw/${Prefix}_${i}_*_L00[0-9]_R1_001.fastq.gz | awk "NR%4==2" | cut -b 1-32 > ${Prefix}_${i}_R1_stripped.txt    zcat raw/${Prefix}_${i}_*_L00[0-9]_R2_001.fastq.gz | awk "NR%4==2" | cut -b 1-20 | paste -d '' ${Prefix}_${i}_R1_stripped.txt - > ${Prefix}_BC${i}_PE.txtdone
+# if you want check CSI, run this.#If everything goes well, most of samples should have correct CSI
+for i in {1..96};do    cut -b 45-52 ${Prefix}_BC${i}_PE.txt | sort | uniq -c | sort -nr > ${Prefix}_BC${i}_CSIcheck.txt    echo ${i}
+    head -5 ${Prefix}_BC${i}_CSIcheck.txt
+done
+#filtering reads only have correct CSI
+for i in {1..96};do	awk "NR==${i}" CSI.txt > CSI	C=$(cut -b 1-8 CSI)	grep "$C"$ ${Prefix}_BC${i}_PE.txt | grep -v N | cut -b 1-44 | sort | uniq -c | sort -nr > ${Prefix}_BC${i}_BU.txt	awk '{print $1}' ${Prefix}_BC${i}_BU.txt > ${Prefix}_BC${i}_BU_counts.txt	awk '{print $2}' ${Prefix}_BC${i}_BU.txt > ${Prefix}_BC${i}_BU_seq.txt
+done
+#Determine threshold using rankplot in matlabmkdir thresholdscd thresholds
+#Set the first position as 0 and give your thresholds from the second position, for example, threshold=(0 3 3 3 3 3 3 3 3)#do a quick collpase around the UMI tags
+for i in {1..96}; do
+    j=${threshold[$i]} 
+    echo ${j}
+    if [ "$j" != "1" ];then
+	awk '$1 < '$j' {print NR}' ../${Prefix}_BC${i}_BU_counts.txt | head -1 >t
+	thresh=$(cat t)
+	echo $thresh
+	head ../${Prefix}_BC${i}_BU_seq.txt -n $thresh | cut -b 1-32 | sort | uniq -c | sort -nr > ${Prefix}_BC${i}_quickout.txt
+   else	cut -b 1-32 ../${Prefix}_BC${i}_BU_seq.txt | sort | uniq -c | sort -nr > ${Prefix}_BC${i}_quickout.txt
+   fi
+done
+#ok, thats a lot of preprocessing done. Now we have to error correct these sequences. The all against all mapping of barcodes in using bowtie is done in the command line. after this we move into MATLAB.
+# this script suppose that you used L4 spikein, if you didn't you should change the sequence 'CGTC' in the below script as your spikein 
+
+mkdir indexes
+for i in {1..96};do	echo ${i}	in=${Prefix}_BC${i}_quickout.txt	grep -v 'CGTCAGTC$' ${in} > ${Prefix}_BC${i}_quickprocessed.txt	awk '{print $1}' ${Prefix}_BC${i}_quickprocessed.txt > ${Prefix}_${i}_counts.txt	awk '{print $2}' ${Prefix}_BC${i}_quickprocessed.txt > ${Prefix}_${i}_seq.txt	nl ${Prefix}_${i}_seq.txt | awk '{print ">" $1 "\n" $2}' > ${Prefix}_${i}fasta2u.txt	bowtie-build -q ${Prefix}_${i}fasta2u.txt indexes/${i}fasta2u	bowtie -v 3 -p 10 -f --best -a indexes/${i}fasta2u ${Prefix}_${i}fasta2u.txt bowtiealignment${i}_2u.txt	awk '{print $1}' bowtiealignment${i}_2u.txt > bowtie${i}_2u_1.txt
+	awk '{print $3}' bowtiealignment${i}_2u.txt > bowtie${i}_2u_3.txt
+done
+#  you want to deal with spike insfor i in {1..96};do	echo ${i}	in=${Prefix}_BC${i}_quickout.txt	grep 'CGTCAGTC$' ${in} > ${Prefix}_spikesBC${i}_quickprocessed.txt	awk '{print $1}' ${Prefix}_spikesBC${i}_quickprocessed.txt > ${Prefix}_spikes${i}_counts.txt	awk '{print $2}' ${Prefix}_spikesBC${i}_quickprocessed.txt > ${Prefix}_spikes${i}_seq.txt	nl ${Prefix}_spikes${i}_seq.txt | awk '{print ">" $1 "\n" $2}' > ${Prefix}_spikes${i}fasta2u.txt	bowtie-build -q ${Prefix}_spikes${i}fasta2u.txt indexes/spikes${i}fasta2u	bowtie -v 3 -p 10 -f --best -a indexes/spikes${i}fasta2u ${Prefix}_spikes${i}fasta2u.txt bowtiealignmentspikes${i}_2u.txt	awk '{print $1}' bowtiealignmentspikes${i}_2u.txt > bowtiespikes${i}_2u_1.txt	awk '{print $3}' bowtiealignmentspikes${i}_2u.txt > bowtiespikes${i}_2u_3.txt
+done
---
+++ scripts/qc_make_readmatrix.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+import argparse
+import logging
+import os
+import sys
+
+from configparser import ConfigParser
+
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+
+gitpath=os.path.expanduser("~/git/mapseq-processing")
+sys.path.append(gitpath)
+
+from mapseq.core import *
+from mapseq.barcode import *
+from mapseq.utils import *
+from mapseq.stats import *
+
+if __name__ == '__main__':
+    FORMAT='%(asctime)s (UTC) [ %(levelname)s ] %(filename)s:%(lineno)d %(name)s.%(funcName)s(): %(message)s'
+    logging.basicConfig(format=FORMAT)
+    logging.getLogger().setLevel(logging.WARN)
+    
+    parser = argparse.ArgumentParser()
+      
+    parser.add_argument('-d', '--debug', 
+                        action="store_true", 
+                        dest='debug', 
+                        help='debug logging')
+
+    parser.add_argument('-v', '--verbose', 
+                        action="store_true", 
+                        dest='verbose', 
+                        help='verbose logging')
+    
+    parser.add_argument('-c','--config', 
+                        metavar='config',
+                        required=False,
+                        default=os.path.expanduser('~/git/mapseq-processing/etc/mapseq.conf'),
+                        type=str, 
+                        help='out file.')    
+
+    parser.add_argument('-s','--sampleinfo', 
+                        metavar='sampleinfo',
+                        required=False,
+                        default=None,
+                        type=str, 
+                        help='Optional XLS sampleinfo file for per-sample changes.')
+
+    parser.add_argument('-S','--samplesheet', 
+                        metavar='samplesheet',
+                        required=False,
+                        default='Sample information',
+                        type=str, 
+                        help='XLS sheet tab name.')
+
+    parser.add_argument('-O','--outdir', 
+                    metavar='outdir',
+                    required=False,
+                    default=None, 
+                    type=str, 
+                    help='outdir. input file base dir if not given.')   
+   
+    parser.add_argument('infile',
+                        metavar='infile',
+                        type=str,
+                        help='Single TSV or parquet read-oriented file')
+        
+    args= parser.parse_args()
+    
+    if args.debug:
+        logging.getLogger().setLevel(logging.DEBUG)
+    if args.verbose:
+        logging.getLogger().setLevel(logging.INFO)   
+
+    cp = ConfigParser()
+    o = cp.read(args.config)
+    if len(o) < 1:
+        logging.error(f'No valid configuration. {args.config}')
+        sys.exit(1)
+       
+    cdict = format_config(cp)
+    logging.debug(f'Running with config. {args.config}: {cdict}')
+    logging.debug(f'infiles={args.infile}')
+          
+    # set outdir / outfile
+    if args.outdir is None:
+        outdir = os.path.abspath('./')
+    else:
+        outdir = os.path.abspath(args.outdir)
+ 
+    os.makedirs(outdir, exist_ok=True)
+    logging.info(f'handling {args.infile} to outdir {outdir}')    
+    logging.debug(f'infile = {args.infile}')
+
+    if args.sampleinfo is not None:
+        logging.debug(f'loading sample DF...')
+        sampdf = load_sample_info(args.sampleinfo, args.samplesheet, cp)
+        logging.debug(f'\n{sampdf}')
+        sampdf.to_csv(f'{outdir}/sampleinfo.tsv', sep='\t')
+    else:
+        logging.info(f'No sampleinfo given. BCXXX column labels. No per-sample spikeins.')
+        sampdf = None    
+       
+    logging.info(f'loading {args.infile}') 
+    df = load_mapseq_df( args.infile, fformat='reads', use_dask=False)
+    logging.debug(f'loaded. len={len(df)} dtypes =\n{df.dtypes}') 
+
+    sldf = qc_make_readmatrix( df,
+                        sampdf=sampdf,
+                        outdir=outdir, 
+                        cp=cp)
+    outfile = os.path.join(outdir, 'EXP.source_reads.tsv')
+    sldf.to_csv(outfile, sep='\t')
+        
+    logging.info(f'Made QC read matrix in {outfile}...')
+
+    
