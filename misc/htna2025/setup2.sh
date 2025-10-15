ca03995be737c55ad26298344246c7a785ba7de8
hover <hover@localhost>
2025-10-14 21:49:33 -0400

    Update.

--- misc/htna2025/setup2.sh
+++ misc/htna2025/setup2.sh
@@ -4,8 +4,8 @@ mkdir fastq ; cd fastq
 wget https://labshare.cshl.edu/shares/mbseq/mapseq/workshops/htna2025/fastq/M205_HZ_S1_R1_001.fastq.gz -O M205_HZ_S1_R1_001.fastq.gz
 wget https://labshare.cshl.edu/shares/mbseq/mapseq/workshops/htna2025/fastq/M205_HZ_S1_R2_001.fastq.gz -O M205_HZ_S1_R2_001.fastq.gz
 cd ~/mapseq
-wget https://raw.githubusercontent.com/ZadorLaboratory/mapseq-processing/refs/heads/main/misc/htna2024/M205.mapseq.conf -O M205.mapseq.conf
-wget https://raw.githubusercontent.com/ZadorLaboratory/mapseq-processing/refs/heads/main/misc/htna2024/M205_sampleinfo.xlsx -O M205_sampleinfo.xlsx
+wget https://raw.githubusercontent.com/ZadorLaboratory/mapseq-processing/refs/heads/main/misc/htna2025/M205.mapseq.conf -O M205.mapseq.conf
+wget https://raw.githubusercontent.com/ZadorLaboratory/mapseq-processing/refs/heads/main/misc/htna2025/M205_sampleinfo.xlsx -O M205.sampleinfo.xlsx
 conda update -y -n base -c defaults conda
 conda config --add channels conda-forge
 conda config --add channels bioconda
--- misc/htna2025/setup3.sh
+++ misc/htna2025/setup3.sh
@@ -3,5 +3,11 @@ conda install -y pandas pyarrow ipython jupyter numpy scikit-learn matplotlib se
 mkdir -p ~/git ; cd ~/git
 git clone https://github.com/ZadorLaboratory/mapseq-processing.git
 git clone https://github.com/ZadorLaboratory/mapseq-analysis.git
-cd 
+git clone https://github.com/jvns/pandas-cookbook.git
+cd pandas-cookbook ; python3 -m venv venv ; source venv/bin/activate ; pip install -r requirements.txt
+
+cd ~/git
+git clone https://github.com/stefmolin/pandas-workshop.git
+cd pandas-workshop ; conda env create -y --file environment.yml 
+cd
 echo "run jupyter notebook, copy URL into browser, navigate to git/mapseq-processing/notebooks"
