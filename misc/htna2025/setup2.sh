715a0d2b1a3ec6b8e5ce43b768949ff6e20433e5
hover <hover@localhost>
2025-10-16 08:41:04 -0400

    Update.

--- misc/htna2025/setup2.sh
+++ misc/htna2025/setup2.sh
@@ -8,11 +8,14 @@ wget https://labshare.cshl.edu/shares/mbseq/mapseq/workshops/htna2025/fastq/M205
 wget https://labshare.cshl.edu/shares/mbseq/mapseq/workshops/htna2025/fastq/M205_HZ_S1_R2_001.fastq.gz -O M205_HZ_S1_R2_001.fastq.gz
 cd ../..
 mkdir output ; cd output
-wget https://labshare.cshl.edu/shares/mbseq/mapseq/workshops/htna2025/M205.htna25.tgz
+wget https://labshare.cshl.edu/shares/mbseq/mapseq/workshops/htna2025/M205.htna25.tgz -O M205.htna25.tgz 
 tar -xvzf M205.htna25.tgz
 
 cd ~/mapseq
 conda update -y -n base -c defaults conda
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
+
 conda config --add channels conda-forge
 conda config --add channels bioconda
 conda create -y -n mapseq python==3.12
