b861a177227f0b99d183cd071f97128e60b79033
hover <hover@localhost>
2025-10-16 08:34:44 -0400

    Update.

--- misc/htna2025/setup2.sh
+++ misc/htna2025/setup2.sh
@@ -2,7 +2,7 @@
 cd ~/mapseq
 mkdir M205 ; cd M205
 wget https://raw.githubusercontent.com/ZadorLaboratory/mapseq-processing/refs/heads/main/misc/htna2025/M205.mapseq.conf -O M205.mapseq.conf
-wget https://raw.githubusercontent.com/ZadorLaboratory/mapseq-processing/refs/heads/main/misc/htna2025/M205_sampleinfo.xlsx -O M205.sampleinfo.xlsx
+wget https://raw.githubusercontent.com/ZadorLaboratory/mapseq-processing/refs/heads/main/misc/htna2025/M205.sampleinfo.xlsx -O M205.sampleinfo.xlsx
 mkdir fastq ; cd fastq
 wget https://labshare.cshl.edu/shares/mbseq/mapseq/workshops/htna2025/fastq/M205_HZ_S1_R1_001.fastq.gz -O M205_HZ_S1_R1_001.fastq.gz
 wget https://labshare.cshl.edu/shares/mbseq/mapseq/workshops/htna2025/fastq/M205_HZ_S1_R2_001.fastq.gz -O M205_HZ_S1_R2_001.fastq.gz
