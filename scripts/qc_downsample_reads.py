d39c089a8a7c98ebae2834d606e0098e8639f74e
hover <hover@la36zada01.cshl.edu>
2025-12-11 12:42:55 -0500

    Update.

--- scripts/qc_downsample_reads.py
+++ scripts/qc_downsample_reads.py
@@ -63,7 +63,7 @@ def qc_process_downsample_all( config_file,
                                force=False,
                                halt=None,
                                #samples=[0.95, 0.90, 0.85, 0.60],
-                               samples=[0.95, 0.90, 0.85, 0.80, 0.75, 0.70, 0.65, 0.60] 
+                               samples=[0.95, 0.90, 0.85, 0.80, 0.75, 0.70, 0.65, 0.60, 0.55, 0.50 ] 
                                ):
     '''
     Downsample and fully process.
